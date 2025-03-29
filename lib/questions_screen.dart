949fc3d04643cf43dd88d501e58c3526867624e1
Mughees <mughees.ahmed243@gmail.com>
2025-03-30 02:57:59 +0500

    connected data with question screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart ';
+import 'package:quizapp/answer_button.dart';
+
+import 'package:quizapp/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +13,48 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text('Questions Screen'));
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            textAlign: TextAlign.center,
+
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          ),
+
+          SizedBox(height: 20),
+
+          AnswerButton(
+            answerText: currentQuestion.answers[0],
+            onTap: () {
+              print('Answer 1 tapped!');
+            },
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[1],
+            onTap: () {
+              print('Answer 2 tapped!');
+            },
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[2],
+            onTap: () {
+              print('Answer 3 tapped!');
+            },
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[3],
+            onTap: () {
+              print('Answer 4 tapped!');
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
